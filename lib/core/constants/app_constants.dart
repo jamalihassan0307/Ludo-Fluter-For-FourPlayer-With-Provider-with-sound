913007550d1dffd29eee9b32d0fda99818d6c1fa
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-03-16 12:11:45 +0500

    Add app, color, and text constants for Ludo Flutter

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,11 @@
+class AppConstants {
+  static const String appName = 'Ludo Flutter';
+  static const String appVersion = '1.0.0';
+  
+  // Route Names
+  static const String splashRoute = '/splash';
+  static const String onboardingRoute = '/onboarding';
+  static const String loginRoute = '/login';
+  static const String homeRoute = '/home';
+  static const String gameRoute = '/game';
+} 
