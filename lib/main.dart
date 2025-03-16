0ad62f599d72a2ad58be18fc5c9c8259050ad853
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-03-16 12:57:28 +0500

    Add routing for onboarding and splash views in the main application file to improve navigation structure.

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:ludo_flutter/core/services/storage_service.dart';
 import 'ui/views/splash/splash_view.dart';
+import 'ui/views/onboarding/onboarding_view.dart';
 import 'viewmodels/splash_viewmodel.dart';
 import 'viewmodels/onboarding_viewmodel.dart';
 
@@ -43,6 +44,19 @@ class Root extends StatelessWidget {
       title: AppConstants.appName,
       navigatorKey: NavigationService.navigatorKey,
       home: const SplashView(),
+      routes: {
+        AppConstants.splashRoute: (context) => const SplashView(),
+        AppConstants.onboardingRoute: (context) => const OnboardingView(),
+        // Add other routes as we create them
+        // AppConstants.loginRoute: (context) => const LoginView(),
+        // AppConstants.homeRoute: (context) => const HomeView(),
+        // AppConstants.gameRoute: (context) => const GameView(),
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (context) => const SplashView(),
+        );
+      },
     );
   }
 }
