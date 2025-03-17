5ae19e07ed9e97ea6e7761e3bd250705ca1b571a
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-03-17 17:33:08 +0500

    update audio

--- lib/audio.dart
+++ lib/audio.dart
@@ -4,26 +4,23 @@ class Audio {
   static AudioPlayer audioPlayer = AudioPlayer();
 
   static Future<void> playMove() async {
-    var duration = await audioPlayer.setAsset('assets/sounds/move.wav');
+    await audioPlayer.setAsset('assets/sounds/move.wav');
     audioPlayer.play();
-    return Future.delayed(duration ?? Duration.zero);
   }
 
   static Future<void> playKill() async {
-    var duration = await audioPlayer.setAsset('assets/sounds/laugh.mp3');
+    await audioPlayer.setAsset('assets/sounds/laugh.mp3');
     audioPlayer.play();
-    return Future.delayed(duration ?? Duration.zero);
   }
 
   static Future<void> rollDice() async {
-    var duration = await audioPlayer.setAsset('assets/sounds/roll_the_dice.mp3');
+    await audioPlayer.setAsset('assets/sounds/roll_the_dice.mp3');
     audioPlayer.play();
-    return Future.delayed(duration ?? Duration.zero);
+    //
   }
 
   static Future<void> playBlockade() async {
-    var duration = await audioPlayer.setAsset('assets/sounds/blocked.wav');
+    await audioPlayer.setAsset('assets/sounds/blocked.wav');
     audioPlayer.play();
-    return Future.delayed(duration ?? Duration.zero);
   }
 }
